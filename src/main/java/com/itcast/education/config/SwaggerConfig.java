04c3a1a2978abbb872c55706a51143489b198b06
zhangzheng521 <2215751025@qq.com>
2020-04-27 13:49:05 +0800

    【ADD】新增Swagger相关使用

--- src/main/java/com/itcast/education/config/SwaggerConfig.java
+++ src/main/java/com/itcast/education/config/SwaggerConfig.java
@@ -19,23 +19,40 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     */
     @Bean
     public Docket createRestApi() {
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                // 选择那些路径和api会生成document
                 .select()
+                // 对所有api进行监控
                 .apis(RequestHandlerSelectors.basePackage("com.itcast.education.controller"))
+                // 对所有路径进行监控
                 .paths(PathSelectors.any())
                 .build();
-
         return docket;
     }
 
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return ApiInfo
+     */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger2构建RESTful APIs")
-                .description("测试文档")
-                .termsOfServiceUrl("http://localhost:8089")
+                // swagger标题
+                .title("CaEducation测试Swagger APIs")
+                // 描述信息
+                .description("CaEducation的后端swagger接口，统一在该平台示例。")
+                // 项目地址
+                .termsOfServiceUrl("http://localhost:8089/education/mediaOutPut?token='test'")
+                // 版本号
                 .version("1.0")
                 .build();
     }
--- src/main/java/com/itcast/education/controller/BaseController.java
+++ src/main/java/com/itcast/education/controller/BaseController.java
@@ -3,6 +3,8 @@ package com.itcast.education.controller;
 import com.itcast.education.controller.dto.HomePageDto;
 import com.itcast.education.model.base.ResponseModel;
 import com.itcast.education.service.MediaOutputService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 基础controller
  * @author zheng.zhang
  */
+@Api(value = "基础接口")
 @RestController
 public class BaseController {
     @Autowired
@@ -24,6 +27,7 @@ public class BaseController {
      * @param categorySize 分类标签展示数量
      * @return
      */
+    @ApiOperation(value = "获取媒体数据信息")
     @GetMapping("/education/mediaOutPut")
     public ResponseModel getMediaOutPut(@RequestParam String token, Integer carouseSize, Integer categorySize) {
         // 封装首页DTO对象
