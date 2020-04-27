cee670a09c8c41b41e69ca8bc71ae6786951a1df
zhangzheng521 <2215751025@qq.com>
2020-04-27 11:14:21 +0800

    【ADD】新增Swagger相关使用

--- src/main/java/com/itcast/education/controller/course/CourseController.java
+++ src/main/java/com/itcast/education/controller/course/CourseController.java
@@ -4,6 +4,7 @@ import com.itcast.education.config.ErrorMessage;
 import com.itcast.education.model.base.ResponseModel;
 import com.itcast.education.model.course.Course;
 import com.itcast.education.service.CourseService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +18,9 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
+    @ApiOperation(value = "添加课程", notes = "传入课程相关信息")
     @PutMapping("/education/addCourse")
-    public ResponseModel addCourse(@RequestParam Course course, @RequestParam String loginId) {
+    public ResponseModel addCourse(Course course, @RequestParam String loginId) {
         boolean result = courseService.saveCourse(course, loginId);
         if (result) {
             return ResponseModel.ok();
