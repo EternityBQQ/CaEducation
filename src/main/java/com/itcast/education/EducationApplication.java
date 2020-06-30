7751b814f7c4006ff2a8f2e3dc38857ddea63a6d
zhangzheng521 <2215751025@qq.com>
2020-06-30 19:08:09 +0800

    【ADD】新增微服务注册功能

--- src/main/java/com/itcast/education/EducationApplication.java
+++ src/main/java/com/itcast/education/EducationApplication.java
@@ -1,11 +1,13 @@
 package com.itcast.education;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching // 开启注解驱动缓存管理
+@NacosPropertySource(dataId = "education-provider.yaml", autoRefreshed = true)
 public class EducationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/itcast/education/config/RegisterConfig.java
@@ -0,0 +1,30 @@
+package com.itcast.education.config;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 向nacos注册服务实例
+ *
+ * @author zheng.zhang
+ * 2020/06/30 18:31:25
+ */
+@Configuration
+public class RegisterConfig {
+    @Value("${server.port}")
+    private int serverPort;
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @NacosInjected
+    private NamingService namingService;
+
+    @PostConstruct
+    public void registerInstance() throws NacosException {
+        namingService.registerInstance(applicationName, "39.97.231.147", serverPort);
+    }
+}
